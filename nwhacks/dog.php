3d395e7a84aa1908f27021b07b2405feb6f40c58
Michael <michaelkxian@gmail.com>
2019-01-26 18:23:10 -0800

    add slackbot command

---
+++ nwhacks/dog.php
@@ -0,0 +1,2 @@
+$slack_webhook_url = "https://hooks.slack.com/services/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"
+$icon_url = "https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg"
