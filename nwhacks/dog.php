d3a02450a7d2a74477ea68089bfdfbc17486a975
Michael <michaelkxian@gmail.com>
2019-01-26 22:25:40 -0800

    Add dog picture

--- nwhacks/dog.php
+++ nwhacks/dog.php
@@ -2,21 +2,16 @@
 $slack_webhook_url = "https://hooks.slack.com/services/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";
 $icon_url = "https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg";
 $dog_url = "https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg";
-$dog_call = curl_init($dog_url);
-$dog_response = curl_exec($dog_call);
-curle_close($dog_call);
+//$dog_call = curl_init($dog_url);
+//$dog_response = curl_exec($dog_call);
+//curl_close($dog_call);
 
 
 $data = array(
-    "username" => "toxic_bot",
-    "channel" => $channel_id,
-    "text" => $message_text,
-    "icon_url" => $icon_url,
     "parse" => "full",
-    "response_type" => "in_channel",
     "text" => $dog_url,
     "attachments" => array(
-        "image_url"=> $dog_url
+        "image_url" => $dog_url
     ),
     "unfurl_media" => true,
     "unfurl_links" => true
